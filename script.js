57cd2e51367ed73f3383852f5edf498d51bd38b9
azoulanoul <83175966+azoulanoul@users.noreply.github.com>
2022-12-24 17:45:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,64 +21,24 @@ function ChangeSlide(sens){
 }
 
 
-// buton color changed
-var button = document.getElementById("btnclick");
-var div = document.getElementById("formcontainer");
+// On sélectionne les éléments HTML
+const form = document.getElementById("my-form");
+const containerForm = document.getElementById("formcontainer");
+const buttonSubmit = document.getElementById("btnclick");
 
-// Set up an event listener for the button click
-button.addEventListener("click", function() {
-  // Replace the content of the div with the message
-  div.innerHTML = "Votre message à bien été envoyé!";
-  div.style.color = "orange";
-  div.style.fontFamily = "outfit, medium";
-  div.style.paddingTop = "10%"
-  div.style.fontSize = "50px"
-  div.style.textAlign = "center";
+// Le formulaire sera exécuté quand on clique sur le btn submit (car il est de type "submit")
+// ET si la validation HTML est ok
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // On remplace le formulaire par un msg de remerciement
+  containerForm.innerHTML = "Votre message à bien été envoyé!";
+  containerForm.style.color = "orange";
+  containerForm.style.fontFamily = "outfit, medium";
+  containerForm.style.paddingTop = "10%";
+  containerForm.style.fontSize = "50px";
+  containerForm.style.textAlign = "center";
+  buttonSubmit.style.display = "none";
 });
-
-// le bouton disparait apres click 
-  var button = document.getElementById("btnclick");
-
-  // Set up an event listener for the button click
-  button.addEventListener("click", function() {
-    // Hide the button
-    button.style.display = "none";
-  });
-
-
-
-  // Get a reference to the form and submit button
-  var form = document.getElementById("formcontainer");
-  var submitButton = document.getElementById("btnclick");
-  var errorMessage = document.getElementById("error-message");
-
-  // Set up an event listener for the submit button click
-  submitButton.addEventListener("click", function(event) {
-    // Prevent the form from being submitted
-    event.preventDefault();
-
-    // Select all of the input elements in the form
-    var inputs = form.querySelectorAll("input");
-
-    // Flag to track whether all of the inputs are filled out
-    var isValid = true;
-
-    // Loop through the inputs
-    inputs.forEach(function(input) { if (input.value === "") {
-      // Set the flag to false
-      isValid = false;
-    }
-  });
-
-  // Check the flag
-  if (isValid) {
-    // All of the inputs are filled out, so you can submit the form
-    form.submit();
-  } else {
-    // One or more of the inputs are empty, so show the error message
-    errorMessage.style.display = "block";
-  }
-});
-
 
 
